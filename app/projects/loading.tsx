5f79c2fa950f7b9748c0d55b4e612530490af9e6
Moa Borg <borg.moa@hotmail.com>
2026-04-16 12:46:57 +0200

    add loading UI for projects routes

---
+++ app/projects/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <section className="py-16">
+      <p className="text-zinc-600 dark:text-zinc-400">Loading projects…</p>
+    </section>
+  );
+}
